ba38c88e8f68e58622eaece4e4ebc88c999b460b
Sandip C <68366475+cymetrix@users.noreply.github.com>
2025-04-14 21:46:56 +0530

    Update Header component to support authentication UI

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  IconButton,
-  Box,
-} from '@mui/material';
+import { AppBar, Toolbar, Typography, IconButton, Box } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AccountCircle from '@mui/icons-material/AccountCircle';
 
 export interface HeaderProps {
   drawerWidth: number;
   onMenuClick: () => void;
+  open?: boolean; // Added to support other versions of MainLayout
+  handleDrawerToggle?: () => void; // Added to support other versions of MainLayout
 }
 
-const Header: React.FC<HeaderProps> = ({ drawerWidth, onMenuClick }) => {
+const Header: React.FC<HeaderProps> = ({ 
+  drawerWidth, 
+  onMenuClick,
+  handleDrawerToggle, // Added to support other versions of MainLayout
+}) => {
+  // Use handleDrawerToggle if provided, otherwise use onMenuClick
+  const handleMenuClick = handleDrawerToggle || onMenuClick;
+
   return (
     <AppBar
       position="fixed"
@@ -28,17 +31,22 @@ const Header: React.FC<HeaderProps> = ({ drawerWidth, onMenuClick }) => {
           color="inherit"
           aria-label="open drawer"
           edge="start"
-          onClick={onMenuClick}
+          onClick={handleMenuClick}
           sx={{ mr: 2, display: { sm: 'none' } }}
         >
           <MenuIcon />
         </IconButton>
         <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
-          Wondrlab Cross-Selling Management
+          Wondrlab Cross-Sell
         </Typography>
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-          <IconButton color="inherit">
-            <AccountCircleIcon />
+        <Box>
+          <IconButton
+            size="large"
+            edge="end"
+            aria-label="account of current user"
+            color="inherit"
+          >
+            <AccountCircle />
           </IconButton>
         </Box>
       </Toolbar>
